244f2747a1d137047cb3db4e93cb967e77cba57b
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2020-02-17 20:57:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CourseShop Server
+Created by [Yuhang Li](https://github.com/WilliamYuhangLee) on Feb 17, 2020.
---
+++ src/test/java/edu/gatech/matcha/courseshop/server/CourseShopApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gatech.matcha.courseshop.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseShopApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
