86cf719c63fe00f424a3181216c2118ad5d2c818
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2020-02-26 09:37:57 -0500

    Change CourseDto's prerequisites and dependents format

--- src/main/java/edu/gatech/matcha/courseshop/server/dto/CourseDto.java
+++ src/main/java/edu/gatech/matcha/courseshop/server/dto/CourseDto.java
@@ -28,8 +28,8 @@ public class CourseDto {
     private String restrictions;
     private List<Long> professors;
     private List<Long> sections;
-    private List<Long> prerequisites;
-    private List<Long> dependents;
+    private List<IdAndCode> prerequisites;
+    private List<IdAndCode> dependents;
 
     public static CourseDto serialize(Course course) {
         return new CourseDto().setId(course.getId())
@@ -50,11 +50,26 @@ public class CourseDto {
                                                  .collect(Collectors.toList()))
                               .setPrerequisites(course.getPrerequisites()
                                                       .stream()
-                                                      .map(Course::getId)
+                                                      .map(IdAndCode::new)
                                                       .collect(Collectors.toList()))
                               .setDependents(course.getDependents()
                                                    .stream()
-                                                   .map(Course::getId)
+                                                   .map(IdAndCode::new)
                                                    .collect(Collectors.toList()));
+    }
+
+    @Data
+    @Accessors(chain = true)
+    @NoArgsConstructor
+    @JsonInclude(value = JsonInclude.Include.NON_NULL)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class IdAndCode {
+        private long id;
+        private String code;
+
+        IdAndCode(Course course) {
+            id = course.getId();
+            code = course.getCode();
+        }
     }
 }
